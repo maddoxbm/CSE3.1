94aeebdeb9467425c308b96ca355d77d3d3a6805
MB <maddox.bekefi-miskolczi@vtsd.com>
2024-03-25 13:00:20 -0400

    3/13/24

---
+++ 3.2.3/dog.py
@@ -0,0 +1,11 @@
+class Dog:
+    #Attributes
+    fur = True
+    legs = 4
+    furColor = "brown"
+
+    #Constructor - Setup every variable
+    def __init__(self, fur, num_of_legs, fur_color):
+        self.fur = fur
+        self.legs = num_of_legs
+        self.fur_color = fur_color
